fbfefbb4d9d23e5c4b5025c1523fe7ffc0212146
sutrejarahul <sutrejarahul0000@gmail.com>
2025-03-08 16:53:24 +0530

    1. monolithic quize application

---
+++ src/main/java/com/example/controller/QuizController.java
@@ -0,0 +1,35 @@
+package com.example.controller;
+
+import com.example.model.QuestionWrapper;
+import com.example.model.Response;
+import com.example.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+// controller for quiz
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
+        return quizService.createQuiz(category, numQ, title);
+    }
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
+    }
+
+
+}
